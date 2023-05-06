eb4ee9073b021a7b887bc5c116a6f1f0de2c334b
keroken <kenichi.shinagawa@gmail.com>
2023-05-06 11:31:45 +0900

    use scale to display chart

--- src/D3Chart.tsx
+++ src/D3Chart.tsx
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 
 const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json';
 
-type AgeData = {
+type Data = {
     name: string,
     height: number,
 };
@@ -14,29 +14,29 @@ export default class D3Chart {
                 .attr("width", 800)
                 .attr("height", 500)
 
-        d3.json<AgeData[]>(url).then((data) => {
+        d3.json<Data[]>(url).then((data) => {
             if (data === undefined) {return};
+
+            const y = d3.scaleLinear()
+                .domain([0, 272])
+                .range([0, 500])
+
+            const x = d3.scaleBand()
+                .domain(data.map(d => d.name))
+                .range([0, 800])
+                .padding(0.2)
 
             const rects = svg.selectAll("rect")
                 .data(data)
         
             rects.enter()
                 .append("rect")
-                    .attr("x", (d, i) => i * 100)
+                    .attr("x", d => x(d.name))
                     .attr("y", 0)
-                    .attr("width", 50)
-                    .attr("height", d => d.height)
-                    .attr("fill", d => {
-                        if (d.height > 10) {
-                            return "red"
-                        }
-                        return "green"
-                    })
+                    .attr("width", x.bandwidth)
+                    .attr("height", d => y(d.height))
+                    .attr("fill", "gray")
 
         })
-    }
-
-    update() {
-
     }
 }
