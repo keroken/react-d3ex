bd66a7920793c36926ec2376254a457e602c1495
keroken <kenichi.shinagawa@gmail.com>
2023-05-06 10:17:58 +0900

    clean up and refactor

--- src/ChartWrapper.tsx
+++ src/ChartWrapper.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import D3Chart from './D3Chart';
 
 export default class ChartWrapper extends Component {
-  componentDidMount(): void {
+  componentDidMount() {
     new D3Chart(this.refs.chart);
   }
 
